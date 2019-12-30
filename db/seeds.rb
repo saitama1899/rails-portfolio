adcf74f8fefba0fee83f0a71459160d96e66a0d4
saitama1899 <eric.lorien92@gmail.com>
2019-12-30 11:16:26 +0100

    Creacion de archivo seed para sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Lo puedes usar para crear datos en la DB durante el desarrollo
+# Para ejecutar rails db:setup
+
+10.times do |blog|
+    Blog.create!(
+        title: "Mi post del Blog # #{blog}", # esta variable nos daria el numero
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Habilidad # #{skill}",
+        percent_utilized: 15
+    )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio|
+    Portfolio.create!(
+        title: "Portfolio # #{portfolio}",
+        subtitle: "Subtitulo del portfolio # #{portfolio}",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        main_image: "http://placehold.it/600x400",
+        thumb_image: "http://placehold.it/350x200"
+    )
+end
+
+puts "9 portfolio created"
+# Para inserciones mas pequeñas se puede usar la consola interactiva
+# de rails c
