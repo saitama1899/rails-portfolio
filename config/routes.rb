4b4a3a3dcb7ad08913fe9a3eef3b83f68af17050
saitama1899 <eric.lorien92@gmail.com>
2019-12-30 13:41:59 +0100

    Personalizada ruta show de portfolio al singular

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  get 'about-me', to 'pages#about'
-  get 'contact', to 'pages#contact'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
-  resources :portfolios 
+  # Para modificar una ruta que viene por defecto, except:
+  resources :portfolios, except: [:show]
+  # La queremos en singular, enlazada al action controller y asignando un nombre
+  # para poder utilizar el enlace en la vista como: portfolio_show_path(portfolio)
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  
+  
   resources :blogs
 
 end
